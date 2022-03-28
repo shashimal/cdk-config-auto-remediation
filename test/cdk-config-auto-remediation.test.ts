16fd7448e57a6983f4106416e40a0d4feb8eba1e
Duleendra Shashimal <duleendra@bambu.co>
2022-03-28 10:47:56 +0800

    Initial commit

---
+++ test/cdk-config-auto-remediation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkConfigAutoRemediation from '../lib/cdk-config-auto-remediation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-config-auto-remediation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkConfigAutoRemediation.CdkConfigAutoRemediationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
